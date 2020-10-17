c726c24bd2c2db8e764fe3cde59489ae16f4bee4
bohma <36440902+bohma@users.noreply.github.com>
2020-10-17 20:14:34 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <MainWrapper />
+  </div>
+</template>
+
+<script>
+import MainWrapper from "./components/MainWrapper.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainWrapper,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Roboto", sans-serif;
+}
+.title {
+  font-weight: 500;
+  font-size: 32px;
+ 
+  &__note {
+    margin-left: 30px;
+    transition: 1s;
+    opacity: 0;
+    font-size: 20px;
+    color: #9c9c9c;
+  }
+}
+.description {
+  color: #727272;
+  &_black{
+    color: #000000;
+  }
+}
+.btn {
+  text-decoration: none;
+  padding: 25px 40px;
+  background: #34547a;
+  color: #ffffff;
+  text-transform: uppercase;
+  transition: 0.3s;
+
+  &:hover {
+    background: #1b416e;
+  }
+}
+img {
+  max-width: 100%;
+}
+.sprite {
+  display: none;
+}
+</style>
